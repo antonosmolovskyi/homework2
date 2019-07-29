62cda28192a05c2dda5dbd9e5cbc30563e207f25
antonosmolovskyi <53257237+antonosmolovskyi@users.noreply.github.com>
2019-07-29 16:03:05 +0200

    homework2.3

--- main.cpp
+++ main.cpp
@@ -1,28 +1,17 @@
-#include <iostream>
+#include<iostream>
 
-int main() {
-  int valuer = 0;
-  int valuer_number = 1;
-  const int TEN = 10;
-  const int ZERO = 0;
-  const int ONE = 1;
-	std::cin >> valuer;
-	int save_to_valuer = valuer;
-	int save_to_valuer1 = valuer;
-	while ((valuer/=TEN) > ZERO) valuer_number++;
-  int first_value = ONE;
-  int second_value = TEN;
-  int counter = ZERO;
-  float average = ZERO;
-
-  for (int i = ZERO; i < valuer_number; ++i) {
-     counter += (save_to_valuer % second_value) / first_value;
-     second_value *= TEN;
-     first_value *= TEN;
-     save_to_valuer = save_to_valuer1;
-     average += i;
+int main () {
+  int num = 0;
+  int temp = 0;
+  const int TWO = 0;
+  std::cout << "Enter count: " << std::endl;
+  std::cin >> num;
+  while (num) {
+    num = num / TWO;
+    temp = num % TWO;
+    std::cout << temp ;
   }
-  std::cout << "summ =  " << counter << std::endl;
-  std::cout << "average = " << counter / average;
+  std::cout << num;
+
   return 0;
 }
